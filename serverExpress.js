2f719a21802ed13ff05a7495cf6f35eea120952f
Julian Lucero <julian.eduardo.lucero@gmail.com>
2022-07-28 15:07:49 -0300

    Entrega 3 servidor Express

---
+++ serverExpress.js
@@ -0,0 +1,38 @@
+const Contenedor = require("./Contenedor");
+const express = require('express');
+const app = express();
+
+/*  Recordar que sea 8080 para glitch.me  */
+const PORT = 8080;
+const contenedor = new Contenedor("productos.json");
+
+app.get('/', (req, res) => {
+    res.send('Esta es la raiz del servidor Express!')
+});
+
+app.get('/productos', async (req, res) => {
+    const allProducts = await contenedor.getAll();
+    res.json(allProducts);
+});
+
+app.get('/productoRandom', async (req, res) => {
+    const allProducts = await contenedor.getAll();
+    const maxId = allProducts.length;
+    
+    const randomNumber = generateRandomNumber(1, maxId);
+    const randomProduct = await contenedor.getById(randomNumber);
+
+    res.json(randomProduct);
+
+})
+
+
+const generateRandomNumber = (min, max) => {
+    return Math.floor((Math.random() * (max+1 -min)) +min);
+}
+
+const server = app.listen(PORT, () => {
+    console.log(`>>>> Servidor iniciado en http://localhost:${PORT}`)
+})
+
+server.on('error', (error) => console.log(error));
